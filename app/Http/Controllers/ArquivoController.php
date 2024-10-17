97bbaacf18f579c89e28bc612edc7ea9a19352ea
Campelo Dev Pro <campelodevpro>
2024-10-16 23:22:29 -0300

    salvando arquivo dentro da pasta, storage_app_private_arquivo

--- app/Http/Controllers/ArquivoController.php
+++ app/Http/Controllers/ArquivoController.php
@@ -15,16 +15,16 @@ class ArquivoController extends Controller
     //
     public function store(Request $request)
     {
-        $request->validate([
-            'arquivo' => 'required|mimes:pdf,docx,xlsx|max:2048',
-        ]);
+        // $request->validate([
+        //     'arquivo' => 'required|mimes:pdf,docx,xlsx|max:2048',
+        // ]);
         // ... (código de validação e armazenamento do arquivo)
         if ($request->hasFile('arquivo')) {
             $arquivo = $request->file('arquivo');
             $nomeArquivo = time().'_'.$arquivo->getClientOriginalName();
         
             // ... (código para armazenar o arquivo)
-        
+            $arquivo->storeAs('uploads', $nomeArquivo);
             Arquivo::create([
                 'nome_original' => $nomeArquivo,
                 'caminho' => 'uploads/' . $nomeArquivo,
